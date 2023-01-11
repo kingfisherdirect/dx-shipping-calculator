944eb9ff209d3cecd6210a15c1aa2e7d29f6ee1c
mattBtindall <matthewbtindall@gmail.com>
2023-01-11 12:10:10 +0000

    Initial commit

---
+++ js/dx-shipping-calculator.js
@@ -0,0 +1,78 @@
+'use strict';
+
+window.onload = function() {
+    const submitBtn = document.querySelector('.submit-btn');
+    const inputs = {
+        height: document.querySelector('input.height'),
+        length: document.querySelector('input.length'),
+        depth: document.querySelector('input.depth'),
+    }
+    const outputs = {
+        volume: document.querySelector('.volume-output'),
+        beforeDiscount: document.querySelector('.before-discount-output'),
+        discount: document.querySelector('.discount-output'),
+        container: document.querySelector('.answer-output')
+    }
+
+    const calcVolume = (height = 0, length = 0, depth = 0) => height * length * depth / 5000;
+
+    const calcBeforeDiscount = (volume) => {
+        let beforeDiscount = 0;
+        if (volume <= 20) {
+            beforeDiscount = 10.50;
+        } else {
+            let answer1, answer2, beforeDiscount = 0;
+            answer1 = volume - 20;
+            answer2 = answer1 * .37;
+            beforeDiscount = answer2 + 10.50;
+        }
+        return beforeDiscount;
+    }
+
+    const calcDiscount = (beforeDiscount) => beforeDiscount * .55;
+
+    const calcAnswers = (height, length, depth) => {
+        const volume = calcVolume(height, length, depth);
+        const beforeDiscount = calcBeforeDiscount(volume);
+        const discount = calcDiscount(beforeDiscount);
+
+        return {
+            volume,
+            beforeDiscount,
+            discount
+        };
+    };
+
+    const outputAnswers = (answers) => {
+        outputs.volume.innerText = answers.volume;
+        outputs.beforeDiscount.innerText = '£' + answers.beforeDiscount;
+        outputs.discount.innerText = '£' + answers.discount;
+    }
+
+    // Event listeners
+    const emptyInputs = () => {
+        // check to see if all inputs are not empty
+        for (const key in inputs) {
+            if (inputs[key].value === "") {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    const updateOuput = () => {
+        if (emptyInputs()) return;
+        const answers = calcAnswers(inputs.height.value, inputs.length.value, inputs.depth.value);
+        outputAnswers(answers);
+    }
+
+    submitBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (!emptyInputs()) outputs.container.classList.add('active');
+        updateOuput();
+    });
+
+    for (const key in inputs) {
+        inputs[key].addEventListener('input', updateOuput);
+    }
+}
